2fab5c738205fd4eb27ca8d6cd6f014cc518f3e8
Suraj <69102843+surajkashyap7@users.noreply.github.com>
2021-08-13 11:35:54 +0530

    Add files via upload

---
+++ Bengaluru_House_PricePrediction/Main.py
@@ -0,0 +1,33 @@
+# flask, scikit-learn, pandas, pickle-mixin
+import pandas as pd
+import numpy as np
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+df = pd.read_csv('cleaned_data.csv')
+pipe = pickle.load(open("RidgeModel.pkl", 'rb'))
+
+
+@app.route('/')
+def index():
+    locations = sorted(df['location'].unique())
+    return render_template('index.html', locations=locations)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    sqft = request.form.get('total_sqft')
+
+    print(location, bhk, bath, sqft)
+    input_ = pd.DataFrame([[location, sqft, bath, bhk]], columns=['location', 'total_sqft', 'bath', 'bhk'])
+    prediction = pipe.predict(input)[0] * 1e5
+
+    return str(np.round(prediction, 2))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
